26e5e791e30ec631bb2d608dce3844e66becd9b1
lkx <leekx430@yahoo.com>
2024-08-30 17:09:43 +0800

    Add project files.

---
+++ ConnectSAP1/Controllers/ConnectSAP.cs
@@ -0,0 +1,45 @@
+﻿using ConnectSAP1.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConnectSAP1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConnectSAP : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Connecting(SAPCompany sap)
+        {
+            try
+            {
+                SAPbobsCOM.Company company = new SAPbobsCOM.Company();
+
+                company.Server = sap.SAPSERVER;
+                company.CompanyDB = sap.SAPDB;
+                company.DbUserName = sap.SQLUSER;
+                company.DbPassword = sap.SQLPASSWORD;
+                if (!string.IsNullOrEmpty(sap.SAPUSER)) company.UserName = sap.SAPUSER; ;
+                if (!string.IsNullOrEmpty(sap.SAPPASSWORD)) company.Password = sap.SAPPASSWORD;
+
+                if (!string.IsNullOrEmpty(sap.SLDSERVER)) company.SLDServer = sap.SLDSERVER;
+                if (!string.IsNullOrEmpty(sap.SAPLiSCENSE)) company.LicenseServer = sap.SAPLiSCENSE;
+
+                company.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL2019;
+
+                int connectionResult = company.Connect();
+
+                if (connectionResult != 0)
+                {
+                    throw new Exception($"Error Code: {company.GetLastErrorCode()}, Description: {company.GetLastErrorDescription()}");
+                }
+
+                return Ok("Connected");
+            }
+            catch (Exception excep)
+            {
+                return BadRequest(excep.Message);
+            }
+
+        }
+    }
+}
---
+++ ConnectSAP1/Model/SAPCompany.cs
@@ -0,0 +1,16 @@
+﻿namespace ConnectSAP1.Model
+{
+    public class SAPCompany
+    {
+        public string SQLUSER { get; set; }
+        public string SQLPASSWORD { get; set; }
+        public string SAPSERVER { get; set; }
+        public string SAPDB { get; set; }
+        public string? SAPUSER { get; set; }
+        public string? SAPPASSWORD { get; set; }
+        //public string? SQLVERSION { get; set; }
+        public string? SLDSERVER { get; set; }
+        public string? SAPLiSCENSE { get; set; }
+        public bool UseTrusted { get; set; } = false;
+    }
+}
